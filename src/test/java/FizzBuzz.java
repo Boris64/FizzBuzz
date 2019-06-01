6b385a2efc8ad5e80d82434bd9f2d7bb139bf14c
Robert <dominant92@yandex.ru>
2019-06-01 20:13:11 +0400

    Change solution

--- src/test/java/FizzBuzz.java
+++ src/test/java/FizzBuzz.java
@@ -5,6 +5,22 @@
 public class FizzBuzz {
     public static void main(String[] args) {
         for (int i = 1; i <= 100; i++) {
+            if ((i % 3 == 0) && (i % 5 ==0)) {
+                System.out.println("FizzBuzz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else {
+                System.out.println(i);
+            }
+            // i % 3 == 0
+        }
+    }
+}
+
+/*
+for (int i = 1; i <= 100; i++) {
             if (i % 3 == 0) {
                 System.out.println("Fizz");
             } else if (i % 5 == 0) {
@@ -14,6 +30,4 @@ public class FizzBuzz {
             } else {
                 System.out.println(i);
             }
-        }
-    }
-}
+ */
